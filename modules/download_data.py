0e8c929974e3ba71014cbeccd6de59995d7b701d
analysispark <analysispark@gmail.com>
2024-09-02 16:01:39 +0900

    배포용_v02

--- modules/download_data.py
+++ modules/download_data.py
@@ -1,7 +1,6 @@
 import gdown
 
-# FILE_ID = "1tdRa4mGWBXTC976LsS146c8o_rmttE1P"
-FILE_ID = "1FTPtF7eIxanm8tQOqqH1eIS_86Q8VH4Q"
+FILE_ID = "1srZ31CdVq6e4IlPT9ZwzzfmEGnkqmE69"
 FILE_NAME = "DATA.zip"
 
 gdown.download(id=FILE_ID, output=FILE_NAME, quiet=False)
